8078e2cce4a9c39f925d938743ed860d0dc34b8c
lvads8 <lvads8@inf.elte.hu>
2022-11-03 14:11:37 +0100

    refactor rest endpoints

--- backend/src/main/java/org/szemeremajax/backend/controllers/GameController.java
+++ backend/src/main/java/org/szemeremajax/backend/controllers/GameController.java
@@ -1,13 +1,8 @@
 package org.szemeremajax.backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.szemeremajax.backend.models.Alliance;
-import org.szemeremajax.backend.models.Board;
-import org.szemeremajax.backend.models.Piece;
+import org.springframework.web.bind.annotation.*;
+import org.szemeremajax.backend.factories.BoardFactory;
 import org.szemeremajax.backend.services.BoardService;
 
 import java.util.UUID;
@@ -18,17 +13,17 @@ public class GameController {
     @Autowired
     BoardService boardService;
 
-    @GetMapping("/create")
+    @PostMapping("/create")
     String create() {
         var id = UUID.randomUUID().toString();
-        var board = new Board(new Piece[50], Alliance.WHITE);
+        var board = BoardFactory.defaultPosition();
         boardService.addBoard(id, board);
 
         return id;
     }
 
-    @GetMapping("/find/[id]")
-    String find(@RequestParam String id) {
+    @GetMapping("/find/{id}")
+    String find(@PathVariable String id) {
         var exists = boardService.lookupBoard(id).isPresent();
         return Boolean.toString(exists);
     }
