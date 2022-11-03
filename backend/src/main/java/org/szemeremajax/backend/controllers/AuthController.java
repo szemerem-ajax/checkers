2fac3735e1e3cc58979bf45c7062797cb0624e2c
lvads8 <lvads8@inf.elte.hu>
2022-11-03 14:12:41 +0100

    add auth controller for creating/joining/leaving games

---
+++ backend/src/main/java/org/szemeremajax/backend/controllers/AuthController.java
@@ -0,0 +1,41 @@
+package org.szemeremajax.backend.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.szemeremajax.backend.models.Alliance;
+import org.szemeremajax.backend.services.AuthService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    AuthService authService;
+
+    @PutMapping("/join/{gameId}/{side}")
+    String join(@PathVariable String gameId, @PathVariable Alliance side) {
+        var result = authService.allocateForGame(gameId, side);
+        return result.orElseThrow();
+    }
+
+    @GetMapping("/free/{gameId}")
+    @ResponseBody
+    FreeResult free(@PathVariable String gameId) {
+        boolean whiteFree = authService.isFree(gameId, Alliance.WHITE);
+        boolean blackFree = authService.isFree(gameId, Alliance.BLACK);
+
+        return new FreeResult(whiteFree, blackFree);
+    }
+
+    @GetMapping("/canStart/{gameId}")
+    boolean canStart(@PathVariable String gameId) {
+        return authService.canBePlayed(gameId);
+    }
+
+    @DeleteMapping("/drop/{gameId}/{side}")
+    void drop(@PathVariable String gameId, @PathVariable Alliance side) {
+        authService.dropAuth(gameId, side);
+    }
+
+    public record FreeResult(boolean white, boolean black) {
+    }
+}
