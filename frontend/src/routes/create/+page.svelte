bddd781b083d0fd9e783fffc6bc21a4e7a9e610c
lvads8 <lvads8@inf.elte.hu>
2022-11-11 12:14:27 +0100

    progress on create and play pages (may change)

---
+++ frontend/src/routes/create/+page.svelte
@@ -0,0 +1,21 @@
+<svelte:head>
+    <title>Checkers - Create</title>
+</svelte:head>
+
+<script lang="ts">
+    const future = fetch('http://localhost:8080/game/create', {
+        method: 'POST',
+    }).then(r => r.text());
+</script>
+
+<div class="grid place-items-center">
+    {#await future}
+        <h1 class="text-3xl">Loading...</h1>
+    {:then res}
+        <h1 class="text-3xl">
+            Success, your game id is: <span class="text-sky-500">{res}</span>.
+        </h1>
+    {:catch _}
+        <h1 class="text-3xl text-rose-500">Something went wrong, please try again.</h1>
+    {/await}
+</div>
---
+++ frontend/src/routes/play/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { enhance } from "$app/forms";
+</script>
+
+<svelte:head>
+    <title>Checkers - Play</title>
+</svelte:head>
+
+<div class="grid place-items-center mb-4">
+    <div class="w-4/5 sm:w-3/5 grid place-items-center">
+        <h1 class="text-5xl font-bold bg-gradient-to-r from-orange-300 to-sky-400 bg-clip-text text-transparent">Play</h1>
+    </div>
+</div>
+<div class="w-full flex flex-wrap justify-center gap-6">
+    <div class="holder">
+        <h1 class="header">Create game</h1>
+        <p class="max-w-prose">You can create a new game by clicking on the button!</p>
+        <a class="btn" href="/create">Create game</a>
+    </div>
+    <div class="w-full md:hidden"></div>
+    <div class="holder">
+        <h1 class="header">Join game</h1>
+        <p class="max-w-prose">You can enter an ID here and join the game!</p>
+        <form action="/join" method="POST" use:enhance>
+			<input class="focus:outline placeholder:italic placeholder:text-slate-400" type="text" name="gameId" id="inputGameId" placeholder="or join an existing game">
+            <button class="btn" type="submit">Join game</button>
+        </form>
+    </div>
+</div>
+
+<style lang="postcss">
+    .header {
+        @apply text-3xl font-semibold;
+    }
+    
+    .holder {
+        @apply grid place-items-center flex-1 gap-1 text-center;
+    }
+    
+    .btn {
+        @apply bg-sky-500 p-2 rounded-md;
+    }
+
+    .btn:hover {
+        @apply bg-sky-400;
+    }
+
+	#inputGameId {
+		@apply bg-slate-800 rounded-md p-2 caret-sky-400 outline-sky-400 outline-1;
+	}
+</style>
